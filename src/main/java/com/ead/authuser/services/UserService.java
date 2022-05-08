1276371962c83577797aeaa18133e5c465cde0ee
Eduardo <eduardomelle@gmail.com>
2022-05-08 12:19:59 -0300

    first commit

---
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -0,0 +1,11 @@
+package com.ead.authuser.services;
+
+import com.ead.authuser.models.UserModel;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserModel> findAll();
+
+}
---
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+}
